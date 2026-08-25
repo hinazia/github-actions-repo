355cdbda6a82c6b9379f718d60f4d835530468c5
hinazia <hinaqazi612@gmail.com>
2026-08-25 02:01:38 +0200

    Python files added

---
+++ .github/status_checker.py
@@ -0,0 +1,19 @@
+# status_checker.py
+
+def check_status(status_code):
+    if status_code == 200:
+        return "Website is up"
+    else:
+        return "Website is down"
+
+
+def main():
+    status = check_status(200)
+    print(status)
+
+    if status != "Website is up":
+        raise SystemExit(1)
+
+
+if __name__ == "__main__":
+    main()
